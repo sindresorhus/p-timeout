67764287c9e79bbf5d87cc3f4c705b16fce4ad0e
Neek Sandhu <neek.sandhu@outlook.com>
2017-11-19 01:08:09 -0800

    Include ms arg typechecking

--- test.js
+++ test.js
@@ -9,6 +9,14 @@ test('resolves before timeout', async t => {
 	t.is(await m(delay(50).then(() => fixture), 200), fixture);
 });
 
+test('throws when ms is not number', async t => {
+	await t.throws(m(delay(50), '200'), TypeError);
+});
+
+test('throws when ms is negative number', async t => {
+	await t.throws(m(delay(50), -1), TypeError);
+});
+
 test('rejects after timeout', async t => {
 	await t.throws(m(delay(200), 50), m.TimeoutError);
 });
